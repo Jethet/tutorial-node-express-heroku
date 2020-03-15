a359b0938d436c40398edbe2128aad0298822c02
Jethet <henriette.hettinga@gmail.com>
2020-03-15 17:37:21 +0100

    Test GET and POST requests, test upload file

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var multer = require("multer");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: "/tmp" }));
 
-app.get("/", function(req, res) {
-  console.log("GET request for the homepage.");
-  res.send("This is GET");
+app.get("/index.html", function(req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
 });
 
-app.post("/", function(req, res) {
-  console.log("POST request for the homepage");
-  res.send("This is POST");
-});
+app.post("/file_upload", function(req, res) {
+  console.log(req.files.file.name);
+  console.log(req.files.file.path);
+  console.log(req.files.file.type);
+  var file = __dirname + "/" + req.files.file.name;
 
-app.delete("/del_user", function(req, res) {
-  console.log("Request to DELETE for /del_user");
-  res.send("This is DELETE");
-});
-
-app.get("/list_user", function(req, res) {
-  console.log("GET request for /list_user");
-  res.send("Page listening");
-});
-
-app.get("/ab*cd", function(req, res) {
-  console.log("GET request for /ab*cd");
-  res.send("Page Pattern Match");
+  fs.readFile(req.files.file.path, function(err, data) {
+    fs.writeFile(file, data, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        response = {
+          message: "File uploaded successfully",
+          filename: req.files.file.name
+        };
+      }
+      console.log(response);
+      res.end(JSON.stringify(response));
+    });
+  });
 });
 
 var server = app.listen(8081, function() {
