89b164d3b00e5ced061343699a4d5c68ac42fe24
Jethet <henriette.hettinga@gmail.com>
2020-10-22 17:02:02 +0200

    Add functionality to get and render passwords; add css

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // initialise state
+  state = { passwords: [] };
+
+  // fetch passwords after first mount
+  componentDidMount() {
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    // get passwords and store them in state
+    fetch("/api/passwords")
+      .then((res) => res.json())
+      .then((passwords) => this.setState({ passwords }));
+  };
+
+  render() {
+    const { passwords } = this.state;
+
+    return (
+      <div className="App">
+        {passwords.length ? (
+          <div>
+            <h1>5 Passwords:</h1>
+            <ul className="passwords">
+              {passwords.map((password, index) => (
+                <li key={index}>{password}</li>
+              ))}
+            </ul>
+            <button className="more" onClick={this.getPasswords}>
+              Get more passwords
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1>There are no passwords, sorry</h1>
+            <button className="more" onClick={this.getPasswords}>
+              Try again?
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
